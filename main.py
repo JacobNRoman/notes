64fa0a590024d016cb490a0f48c6e53301dd9057
Jacob Roman <jnroman7@gmail.com>
2017-10-27 13:26:34 -0500

    Added profile pages and following/unfollowing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, session, flash, g, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import hashlib
@@ -11,6 +11,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://notes:lcproject@localho
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y33kGcyk&P3B'
+
+#followers is an association table that sets up a self-referential many-to-many relationship in the Users table, allowing us to track what Users a given user is following and who follows a given User. 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class Note(db.Model):
 
@@ -37,11 +43,38 @@ class User(db.Model):
     name = db.Column(db.String(120))
     username = db.Column(db.String(120), unique=True)
     pw_hash = db.Column(db.String(120))
+    about_me = db.Column(db.String(140))
     notes = db.relationship('Note', backref='owner')
+    followed = db.relationship('User',
+        #'User' arg above defines what the right-side table is. Here it is still User. 
+        secondary=followers,
+        #secondary defines what association table to use, in this case followers
+        primaryjoin=(followers.c.follower_id == id),
+        #primary join determines what condition links the left side table
+        secondaryjoin=(followers.c.followed_id == id),
+        #secondary join determines what condition links the right side table
+        backref=db.backref('followers', lazy='dynamic'),
+        #the backref here defines how the relationship is accessed when coming from the right side, which would give as a list of a Users followers. 
+        lazy='dynamic')
 
     def __init__(self, email, password):
         self.email = email
         self.pw_hash = make_pw_hash(password)
+    
+    #follow and unfollow methods are written below. We include them here to keep them separate from our view functions, in keeping with MVC. They return an object when they succeed and None when they fail. Keep in mind that even when they return an object, that object msut still be added to the database session and committed. 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+    
+    #is_following takes a user as an argument, and the runs a query of all users you have followed, filtered by the id of the specific user. Any return is a success, None is a failure. 
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count()
 
 def make_salt():
     return "".join([random.choice(string.ascii_letters) for x in range(5)])
@@ -63,6 +96,15 @@ def require_login():
     allowed_routes = ['login', 'register']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
+
+@app.before_request
+# load_user gives a persistent global variable containing the current user whenever they are logged in. It requires the g module from flask, which I need to research more carefully. Also, doing it this way isn't terribly secure, as you are constantly passing the user's email through the session. Flask-Login apparently contains a more secure approach. 
+def load_user():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+    else:
+        user = {"name":"Guest"}
+    g.user = user
 
 
 @app.route('/login', methods=['POST', 'GET'] )
@@ -122,9 +164,11 @@ def newAccount():
         user = User.query.filter_by(email=session['email']).first()
         name = request.form['name']
         username = request.form['username'] 
+        about_me = request.form['aboutme']
         #TODO check if username already exists, and if so send an error
         user.name = name
         user.username = username
+        user.about_me = about_me
         db.session.commit()
         # TODO: add birthday
         return redirect("/")
@@ -141,16 +185,23 @@ def index():
     notes = Note.query.all()
     return render_template('index.html', notes=notes)
 
-@app.route("/profile")
-def profile():
-    user = User.query.filter_by(email=session['email']).first()
+@app.route("/users")
+def users():
+    # Users simply renders a list of all users. TODO - replace this with a better user browsing and searching solution
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    user = User.query.filter_by(username=username).first()
     notes = Note.query.filter_by(owner_id=user.id).all()
     return render_template('profile.html', user=user, notes=notes)
  
-
+ 
 @app.route('/newnote', methods=['POST', 'GET'])
 def newNote():
-    owner = User.query.filter_by(email=session['email']).first()
+    current_user = User.query.filter_by(email=session['email']).first()
     if request.method == 'POST':
         # the indented code below handles the submission and creation of a new post
         note = request.form['note']
@@ -158,16 +209,50 @@ def newNote():
             #TODO validate so that you get an error message if note is over 140 chars
             flash("You didn't write anything!", "error")
             return redirect ('/newnote')
-        new_note = Note(note, owner)
+        new_note = Note(note, current_user)
         db.session.add(new_note)
         db.session.commit()
         new_note_id = new_note.id
         note = Note.query.filter_by(id=new_note_id).first()
-        user = User.query.filter_by(id=note.owner_id).first()
         # after a new post is submitted the user is redirected to that new post's individual page. 
-        return render_template("note.html", note=note, user=user)
-    return render_template('newnote.html')
+        return render_template("note.html", note=note, current_user=current_user)
+    return render_template('newnote.html', current_user=current_user)
 
+@app.route('/follow/<username>')
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {0} not found.'.format(username))
+        return redirect('/')
+    if user == g.user:
+        flash("You can't follow yourself!")
+        return redirect(url_for('profile', username=username))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + username)
+        return redirect(url_for('profile', username=username))
+    db.session.add(u)
+    db.session.commit()
+    flash("You are now following " + username)
+    return redirect(url_for('profile', username=username))
+
+@app.route('/unfollow/<username>')
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("User {0} not found".format(username))
+        return redirect(url_for('profile', username=username))
+    if user == g.user:
+        flash("You can't unfollow yourself!")
+        return redirect(url_for("profile", username=username))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow' + username)
+        return redirect(url_for('profile',username=username))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + username)
+    return redirect(url_for('profile', username=username))
 
 @app.route('/delete', methods=['POST'])
 def delete_post():
